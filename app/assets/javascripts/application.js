dbe3dcabee5ff8f4465cc34bb5f847ff743c7f93
Sumayyah Perine <sperine01@gmail.com>
2016-04-03 12:16:09 -0400

    add some functionality for map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,25 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+var markersArray = [];
+var nycLat = 40.7142700;
+var nycLong= -74.0059700;
+var theQuery = 400;
+var inactive = false;
+
+// 
+$(document).ready(function() {
+	mapItUp();
+});
+
+var mapItUp = function() {
+	var myMapOptions = {
+		center: new google.maps.LatLng(nycLat, nycLong),
+		zoom: 12,
+		streetViewControl: true,
+		zoomControl:false,
+
+	}
+};
